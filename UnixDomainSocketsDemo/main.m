c365f48cd9c0270796e1c931877c8283dcb3ce2b
Jerry Krinock <jerry@sheepsystems.com>
2021-07-20 16:36:20 -0700

    Initial Commit

---
+++ UnixDomainSocketsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnixDomainSocketsDemo
+//
+//  Created by Jerry on 21-07-20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
